bc1074468279d80d9c7d7c88ef0971335f9dd26f
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-19 05:49:20 +0700

    tambahin variable untuk local font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Rubik_Glitch } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import SmoothScroll from "@/src/components/SmoothScroll";
 import "./globals.css";
+import localFont from "next/font/local";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,10 +14,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const rubikGlitch = Rubik_Glitch({
-  weight: "400",
-  variable: "--font-rubik-glitch",
-  subsets: ["latin"],
+const helvetica = localFont({
+  src: [
+    {
+      path: "./fonts/helvetica-now-display/HelveticaNowDisplay-ExtLt.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/helvetica-now-display/HelveticaNowDisplay-Black.woff2",
+      style: "normal",
+    },
+    {
+      path: "./fonts/helvetica-now-display/HelveticaNowDisplay-Regular.woff2",
+      style: "normal",
+    },
+  ],
+  variable: "--font-helvetica",
+  display: "swap",
+});
+
+const nb = localFont({
+  src: "./fonts/nb-font/NB International Regular Webfont.ttf",
+  variable: "--font-nb",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -31,9 +51,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${helvetica.variable} ${nb.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${rubikGlitch.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
       >
         <SmoothScroll>{children}</SmoothScroll>
       </body>

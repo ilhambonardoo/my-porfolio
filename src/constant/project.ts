1a5b90bd903e54ab584b9c78f98a3807fbc5f582
ilhambonardoo <ilhambonardo123@gmail.com>
2026-06-11 01:46:06 +0700

    tambahin asset yang ingin dimasukan

--- src/constant/project.ts
+++ src/constant/project.ts
@@ -27,11 +27,30 @@ export const projects = [
   },
   {
     id: 3,
-    title: "Chronos AI",
+    title: "One Fish",
     description:
-      "Conversational AI interface with generative UI components. Features streaming responses and context-aware component rendering.",
-    tech: ["OpenAI API", "Tailwind CSS", "Prisma", "Redis"],
-    image: "https://placehold.co/800x600/18181b/ffffff?text=AI+Interface",
+      "Website portfolio toko ikan One Fish. Menyelam ke dalam koleksi curated keunggulan akuatik. Dipilih dengan cermat untuk memastikan vitalitas yang tak tertandingi dan keindahan yang menakjubkan.",
+
+    tech: ["NodeJS", "Tailwind CSS", "Next.js", "Cloudinary"],
+    image: "/project/onefish.png",
+    Link: "https://onefish.vercel.app/",
+    bg: "bg-zinc-950",
+  },
+  {
+    id: 4,
+    title: "Smart Parking Light",
+    description:
+      "Membuat sistem parkir otomatis yang berguna untuk menghitung jumlah mobil yang sudah masuk dalam 1 hari. Selain itu, parkiran tersebut memberikan informasi jika lampu warna merah maka parkiran tersebut telah terisi. Kemudian, jika warna hijau parkiran kosong",
+
+    tech: [
+      "Seven Segment",
+      "Kabel Jumper",
+      "LDR (Light Depentdent Resistor)",
+      "IC 7404",
+      "LED",
+    ],
+    image: "/project/smart.jpeg",
+    Link: "/video/smart.mp4",
     bg: "bg-zinc-950",
   },
 ];

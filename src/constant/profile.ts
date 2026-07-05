a7b4ce8b9a309d4c0dd607ec4e158542300294f4
ilhambonardoo <ilhambonardo123@gmail.com>
2026-07-05 16:11:14 +0700

    Memperbarui tampilan profile agar lebih proper

--- src/constant/profile.ts
+++ src/constant/profile.ts
@@ -1,36 +1,30 @@
-export const techStack = [
-  "TypeScript",
-  "Next.js",
-  "React",
-  "Tailwind CSS",
-  "Node.js",
-  "Firebase",
-  "Prisma",
-  "GSAP",
-];
-
-export const experiences = [
+export const skills = [
   {
-    period: "2024 - Sekarang",
-    title: "Freelance Frontend Developer",
-    description:
-      "Membangun website company profile dan landing page dengan fokus pada performa, SEO, dan pengalaman pengguna.",
+    title: "Hardware & Software Engineering",
+    level: "Advance",
   },
   {
-    period: "2023 - 2024",
-    title: "Project Developer - IoTani",
-    description:
-      "Mengembangkan sistem monitoring pH dan kelembaban tanah berbasis website untuk otomasi irigasi pada tanaman cabai.",
+    title: "Team Collaboration",
+    level: "Advance",
   },
   {
-    period: "2022 - 2023",
-    title: "Web Developer Learner",
-    description:
-      "Mempelajari fondasi web modern mulai dari HTML, CSS, JavaScript hingga membangun aplikasi dengan React dan Next.js.",
+    title: "Microcontroller Programming (ESP32/ESP8266/Arduino)",
+    level: "Advance",
   },
   {
-    period: "2023 - 2024",
-    title: "Juara Basket Turnamen Olimpiade Mahasiswa IPB",
-    description: "Melatih fisik dan kemampuan untuk bekerja sama antar tim.",
+    title: "IoT (Internet Of Things)",
+    level: "Advance",
+  },
+  {
+    title: "Adaptability ",
+    level: "Advance",
+  },
+  {
+    title: "Machine Learning ",
+    level: "Advance",
+  },
+  {
+    title: "Full Stack Web Development",
+    level: "Advance",
   },
 ];

8eb51b9a1a5b05bfe6e249198ff24f37be7aeb0a
ilhambonardoo <ilhambonardo123@gmail.com>
2026-07-12 22:36:14 +0700

    Mengubah xPercentnya menjadi -120

--- src/hooks/useProjectAnimation.ts
+++ src/hooks/useProjectAnimation.ts
@@ -12,7 +12,7 @@ export const UseProjectAnimation = (
   const totalPanels = projectsLength + 1;
 
   const scrollTween = gsap.to(trackElement, {
-    xPercent: -100 * (totalPanels - 1),
+    xPercent: -120 * (totalPanels - 1),
     ease: "none",
     scrollTrigger: {
       trigger: trackElement.parentElement,
